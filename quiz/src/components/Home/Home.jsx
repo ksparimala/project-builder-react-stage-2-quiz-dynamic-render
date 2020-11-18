617d16bdce600ad1ddba1f9c2b8f1d92d8656b13
ksparimala <parimala@DESKTOP-QIPH3QC.localdomain>
2020-11-19 02:09:03 +0530

    done

---
+++ quiz/src/components/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './Home.css';
+
+class Home extends Component {
+    render() {
+        return (
+            <div class="quiz">
+                <p>Quiz App</p>
+                <button>Play</button>
+            </div>
+        );
+    }
+}
+
+export default Home;
